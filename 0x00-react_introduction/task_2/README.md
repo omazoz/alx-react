37842b52defe9c7258e92f72637327558417811e
omazoz <oumaymamazoz12@gmail.com>
2024-02-02 17:55:04 +0100

    all done

---
+++ 0x00-react_introduction/task_2/README.md
@@ -0,0 +1,2 @@
+here is task 2
+
---
+++ 0x00-react_introduction/task_3/dashboard/src/setupTests.js
@@ -0,0 +1,7 @@
+
+// task_3/dashboard/src/setupTests.js
+
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-17-updated';
+
+Enzyme.configure({ adapter: new Adapter() });
